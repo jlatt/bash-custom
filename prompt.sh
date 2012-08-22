ae127398e0669b6cf3f65a3cc60c4e5c38bbbba6
Jeremy Latt <jeremy@verticalbrands.com>
2012-08-22 13:55:05 -0700

    Make sure the prompt command uses git and not hub.

--- prompt.sh
+++ prompt.sh
@@ -3,14 +3,16 @@ source_custom colors
 
 set -a
 
+git="$(which git)"
+
 function maybe_git_branch() {
-	branch_name="$(git branch-name)"
+	branch_name="$($git branch-name)"
 	if [[ "$branch_name" ]]; then
         prompt="${YELLOW}b${NORMAL}:${AQUA}${branch_name}${NORMAL}"
-        upstream_branch="$(git upstream-branch)"
+        upstream_branch="$($git upstream-branch)"
         if [[ "$upstream_branch" ]]; then
             merge="m"
-            if [[ "$(git config --bool branch.${branch_name}.rebase)" == 'true' ]]; then
+            if [[ "$($git config --bool branch.${branch_name}.rebase)" == 'true' ]]; then
                 merge="r"
             fi
             upstream_branch="${upstream_branch/%${branch_name}/*}"
