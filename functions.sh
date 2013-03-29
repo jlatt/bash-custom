ff0c53fda587badff99ca52dffd7629374e30b59
Jeremy Latt <jlatt@apartmentlist.com>
2013-03-29 09:45:41 -0700

    Don't protect function defs with a variable.

--- functions.sh
+++ functions.sh
@@ -1,45 +1,44 @@
 #!/bin/bash
-if [[ -z "$bash_custom_functions" ]]; then
-	# append a path to $PATH
-	function append_path() {
-		[[ -d "$1" ]] || return 1
-		if [[ "$PATH" ]]; then
-			PATH="$PATH:$1"
-		else
-			PATH="$1"
-		fi
-	}
 
-	# prepend a path to $PATH
-	function prepend_path() {
-		[[ -d "$1" ]] || return 1
-		if [[ "$PATH" ]]; then
-			PATH="$1:$PATH"
-		else
-			PATH="$1"
-		fi
-	}
+# append a path to $PATH
+function append_path() {
+	[[ -d "$1" ]] || return 1
+	if [[ "$PATH" ]]; then
+		PATH="$PATH:$1"
+	else
+		PATH="$1"
+	fi
+}
 
-	# Conditionally source a file, if it exists.
-	function source_file() {
-		[[ -r "$1" ]] || return 1
-		source "$1"
-	}
+# prepend a path to $PATH
+function prepend_path() {
+	[[ -d "$1" ]] || return 1
+	if [[ "$PATH" ]]; then
+		PATH="$1:$PATH"
+	else
+		PATH="$1"
+	fi
+}
 
-	CUSTOM_DIR="$(dirname ${BASH_ARGV[0]})"
+# Conditionally source a file, if it exists.
+function source_file() {
+	[[ -r "$1" ]] || return 1
+	source "$1"
+}
 
-	function source_custom() {
-		source_file "$CUSTOM_DIR/$1.sh"
-	}
+CUSTOM_DIR="$(dirname ${BASH_ARGV[0]})"
 
-    function has_executable() {
-        which -s "$@"
-    }
+function source_custom() {
+	source_file "$CUSTOM_DIR/$1.sh"
+}
 
-    function time_call() {
-        echo "timing $@"
-        time "$@"
-    }
+function has_executable() {
+    which -s "$@"
+}
 
-	export bash_custom_functions="true" append_path prepend_path source_file source_custom has_executable
-fi
+function time_call() {
+    echo "timing $@"
+    time "$@"
+}
+
+export append_path prepend_path source_file source_custom has_executable
